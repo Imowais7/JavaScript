75a8189a6d4615e401f69e8d5b4c3eb8fba54b84
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-20 17:29:07 +0530

    Update and rename 2_.js to 2_table.js

---
+++ 2_table.js
@@ -0,0 +1,8 @@
+//creating a table 
+
+const accountId=123456
+let accountEmail= "owaisansari9860@gmail.com"
+var accountPassword ="11111"
+accountCity = "Pune"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])

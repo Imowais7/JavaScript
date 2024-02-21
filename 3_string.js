0c2a6f87e35750331e7632840a08704d5b01f6b2
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-21 16:39:32 +0530

    Add files via upload

---
+++ 3_string.js
@@ -0,0 +1,18 @@
+const name = "owais"
+const age = 20
+// console.log (name + Count +"value");
+console.log(`Hello my name is ${name} and my age is ${age} `);
+
+const gameName= new String('owais')
+
+console.log(gameName[3])
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('a'));
+
+const newString1 =gameName.substring(0,4)
+console.log(newString1)
+
+const newString2="           owais                               "
+console.log(newString2)
+console.log(newString2.trim())

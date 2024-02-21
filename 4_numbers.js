740e5e1db07b9ce6f5e35f6d7a43eca1ab4402a6
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-21 18:30:49 +0530

    Add files via upload

---
+++ 4_numbers.js
@@ -0,0 +1,19 @@
+const score =400
+
+const balance = new Number(1000)
+console.log(balance)
+console.log(balance.toFixed(3))
+console.log(balance.toString().length)
+const otherNumber =1224.364745
+console.log(otherNumber.toPrecision(6))
+
+console.log("****************************maths****************************")
+
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.7))
+console.log(Math.min(2,4,7,9,12))
+console.log(Math.max(2,4,7,229,12))
+
+let a
+console.log(Math.floor(Math.random()*6) + 1)

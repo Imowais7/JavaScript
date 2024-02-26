4213fc1c077806c1386171a618eb05786f49fb78
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-27 00:24:00 +0530

    Add files via upload

---
+++ 9_function.js
@@ -0,0 +1,19 @@
+function sayMyName(){
+    console.log("owais")
+}
+sayMyName()
+
+function addNumbers(number1 , number2){
+    let result=number1+number2
+    return result
+}
+const result=addNumbers(3,4)
+console.log(result)
+
+
+function loginUserMassage(username){
+    return`${username} just logged in`
+}
+// const result2=loginUserMassage("Ansari")
+// console.log(result2)
+console.log(loginUserMassage("ansari"))

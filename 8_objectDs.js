b934d387dc2c640fe48c77c00b42651abce31b59
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-26 23:33:23 +0530

    Add files via upload

---
+++ 8_objectDs.js
@@ -0,0 +1,14 @@
+const course= {
+    coursename:"javaScript",
+    price:"5999",
+    courseInstructure:"owais"
+}
+// course.courseInstructure
+const{courseInstructure}=course
+console.log(courseInstructure)
+
+// {
+//     "name":"owais",
+//     "courseName":"js hindi",
+//     "price": "499"
+// }

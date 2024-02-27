56bdff2b749730f4b5618dcdffe74f8dbc65f839
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-28 00:55:40 +0530

    Add files via upload

---
+++ 10_functionWithObject.js
@@ -0,0 +1,19 @@
+function calculateCarPrice(...num1){
+    return num1
+}
+console.log(calculateCarPrice(200, 400, 600, 800))
+const user = {
+ name:"owais",
+ price:300
+ 
+}
+function handleObject(anyobject){
+    console.log(`username is${anyobject.name} and price is ${anyobject.price}`)
+}
+handleObject(user)
+
+const myArray=[200,300,400,500]
+function returnValue(getarray){
+    return getarray[1]
+}
+console.log(returnValue(myArray))

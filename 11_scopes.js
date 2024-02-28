8e1592e7eb06dc8d04e2ead0cb213688ac7eb0ba
Owais Shakil Ansari <97333101+Imowais7@users.noreply.github.com>
2024-02-29 00:52:26 +0530

    Add files via upload

---
+++ 11_scopes.js
@@ -0,0 +1,29 @@
+function one(){
+    const username="owais"
+    function two(){
+        const website ="youtube"
+        console.log(username )
+    }
+    // console.log(website)
+    two()
+}
+one()
+
+if (true)
+{
+    const username="owais"
+    if(username ==="owais")
+    {
+        const website="youtube"
+        console.log(username+website)
+    }
+}
+
+function addone(num){
+    return num+1
+}
+addone(5)
+const addtwo=function(num){
+    return num+2
+}
+console.log(addtwo(8))
